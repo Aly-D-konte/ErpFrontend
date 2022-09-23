2b3fdf7cd368ed5fe13e9ed60c788e7c846c652b
nurdev1 <fatoudevweb1@gmail.com>
2022-09-23 12:54:59 +0000

    Les modeles

--- ErpFrontendAngular/src/app/app.module.ts
+++ ErpFrontendAngular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderAdminComponent } from './header-admin/header-admin.component';
+import { TirageComponent } from './tirage/tirage.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderAdminComponent,
+    TirageComponent,
+    AuthentificationComponent
   ],
   imports: [
     BrowserModule,
