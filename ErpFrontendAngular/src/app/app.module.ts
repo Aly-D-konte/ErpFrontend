0cfaf5f780d55ce4ee1c3d2533dd7a9d29c95ab8
adkonte <alykonte19@gmail.com>
2022-09-23 14:20:11 +0000

    component accueil

--- ErpFrontendAngular/src/app/app.module.ts
+++ ErpFrontendAngular/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderAdminComponent } from './header-admin/header-admin.component';
 import { TirageComponent } from './tirage/tirage.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
+import { FormsModule } from '@angular/forms';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ActeurAdminComponent } from './acteur-admin/acteur-admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderAdminComponent,
     TirageComponent,
-    AuthentificationComponent
+    AuthentificationComponent,
+    AccueilComponent,
+    ActeurAdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
